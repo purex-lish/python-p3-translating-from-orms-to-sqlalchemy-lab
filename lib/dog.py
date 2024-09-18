bf2590e999a635085423568cde597af8d3eb9081
Purity Wambua <puritywamb@gmail.com>
2024-09-18 09:35:50 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,36 @@
-from models import Dog
+from sqlalchemy import create_engine
+from models import Dog, Base
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+def create_table(base, engine ):
+    """Create the database tables."""
+    #engine = create_engine(database_url)
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    """Save a Dog instance to the database."""
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    """Return all Dog instances from the database."""
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    """Find a Dog by its name."""
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    """Find a Dog by its ID."""
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    """Find a Dog by its name and breed."""
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    """Update a Dog's breed."""
+    dog.breed = breed
+    session.commit()
+
+
